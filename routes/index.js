3a531f049509fab5ff43b79761f51ee8190ac6a8
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2016-11-26 14:49:44 -0500

    Adding in things for Twilio

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,17 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-// our db model
-var Animal = require("../models/model.js");
+//Twilio
+var twilio = require('twilio');
 
-// simple route to render am HTML form that can POST data to our server
-// NOTE that this is not a standard API route, and is really for testing
-router.get('/create-pet', function(req,res){
-  res.render('pet-form.html')
-})
+// Geocoder
+var geocoder = require('geocoder');
 
-// simple route to render an HTML page that pulls data from our server and displays it on a page
-// NOTE that this is not a standard API route, and is really for testing
-router.get('/show-pets', function(req,res){
-  res.render('show-pets.html')
-})
+// our db model1
+var Status = require("../models/status.js");
+
+// our db model2
+var Meal = require("../models/meal.js");
 
 /**
  * GET '/'
@@ -24,285 +21,215 @@ router.get('/show-pets', function(req,res){
  * @return {Object} json
  */
 router.get('/', function(req, res) {
-  
+
   var jsonData = {
-  	'name': 'node-express-api-boilerplate',
-  	'api-status':'OK'
+  	'name': 'node-express-twilio-sms',
+  	'api-status':'OK',
+    'instructions': "Text your meal to (678)-264-6646",
+    'format': 'type,rating,place,location'
   }
 
   // respond with json data
   res.json(jsonData)
 });
 
-// simple route to show an HTML page
-router.get('/sample-page', function(req,res){
-  res.render('sample.html')
-})
+// Twilio callback route
+// This gets called every time an incoming message is received
+router.post('/twilio-callback', function(req,res){
 
-// /**
-//  * POST '/api/create'
-//  * Receives a POST request of the new animal, saves to db, responds back
-//  * @param  {Object} req. An object containing the different attributes of the Animal
-//  * @return {Object} JSON
-//  */
+  // there's lots contained in the body
+  console.log(req.body);
 
-router.post('/api/create', function(req, res){
+  // the actual message is contained in req.body.Body
+  var incomingMsg = req.body.Body;
+  console.log(incomingMsg);
 
-    console.log(req.body);
+  var incomingNum = req.body.From;
 
-    // pull out the information from the req.body
-    var name = req.body.name;
-    var age = req.body.age;
-    var tags = req.body.tags.split(","); // split string into array
-    var weight = req.body.weight;
-    var color = req.body.color;
-    var url = req.body.url;
+  // now, let's save it to our Database
+  var msgToSave = {
+    status: incomingMsg,
+    from: incomingNum
+  }
 
-    // hold all this data in an object
-    // this object should be structured the same way as your db model
-    var animalObj = {
-      name: name,
-      age: age,
-      tags: tags,
-      description: {
-        weight: weight,
-        color: color
-      },
-      url: url
-    };
+  var status = new Status(msgToSave)
 
-    // create a new animal model instance, passing in the object
-    var animal = new Animal(animalObj);
-
-    // now, save that animal instance to the database
-    // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model-save    
-    animal.save(function(err,data){
-      // if err saving, respond back with error
-      if (err){
-        var error = {status:'ERROR', message: 'Error saving animal'};
-        return res.json(error);
-      }
-
-      console.log('saved a new animal!');
-      console.log(data);
-
-      // now return the json data of the new animal
-      var jsonData = {
-        status: 'OK',
-        animal: data
-      }
-
-      return res.json(jsonData);
-
-    })  
-});
-
-// /**
-//  * GET '/api/get/:id'
-//  * Receives a GET request specifying the animal to get
-//  * @param  {String} req.params.id - The animalId
-//  * @return {Object} JSON
-//  */
-
-router.get('/api/get/:id', function(req, res){
-
-  var requestedId = req.params.id;
-
-  // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model.findById
-  Animal.findById(requestedId, function(err,data){
-
-    // if err or no user found, respond with error 
-    if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find that animal'};
-       return res.json(error);
-    }
-
-    // otherwise respond with JSON data of the animal
-    var jsonData = {
-      status: 'OK',
-      animal: data
-    }
-
-    return res.json(jsonData);
-  
-  })
-})
-
-// /**
-//  * GET '/api/get'
-//  * Receives a GET request to get all animal details
-//  * @return {Object} JSON
-//  */
-
-router.get('/api/get', function(req, res){
-
-  // mongoose method to find all, see http://mongoosejs.com/docs/api.html#model_Model.find
-  Animal.find(function(err, data){
-    // if err or no animals found, respond with error 
-    if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find animals'};
-      return res.json(error);
-    }
-
-    // otherwise, respond with the data 
-
-    var jsonData = {
-      status: 'OK',
-      animals: data
-    } 
-
-    res.json(jsonData);
-
-  })
-
-})
-
-// /**
-//  * GET '/api/search'
-//  * Receives a GET request to search an animal
-//  * @return {Object} JSON
-//  */
-router.get('/api/search', function(req,res){
-
-  // first use req.query to pull out the search query
-  var searchTerm = req.query.name;
-  console.log("we are searching for " + searchTerm);
-
-  // let's find that animal
-  Animal.find({name: searchTerm}, function(err,data){
-    // if err, respond with error 
+  status.save(function(err,data){
+    // set up the twilio response
+    var twilioResp = new twilio.TwimlResponse();
     if(err){
-      var error = {status:'ERROR', message: 'Something went wrong'};
-      return res.json(error);
+      // respond to user
+      twilioResp.sms('Oops! We couldn\'t save status --> ' + incomingMsg);
+      // respond to twilio
+      res.set('Content-Type', 'text/xml');
+      res.send(twilioResp.toString());
     }
-
-    //if no animals, respond with no animals message
-    if(data==null || data.length==0){
-      var message = {status:'NO RESULTS', message: 'We couldn\'t find any results'};
-      return res.json(message);      
+    else {
+      // respond to user
+      twilioResp.sms('Successfully saved status! --> ' + incomingMsg);
+      // respond to twilio
+      res.set('Content-Type', 'text/xml');
+      res.send(twilioResp.toString());
     }
-
-    // otherwise, respond with the data 
-
-    var jsonData = {
-      status: 'OK',
-      animals: data
-    } 
-
-    res.json(jsonData);        
   })
+
 
 })
 
-// /**
-//  * POST '/api/update/:id'
-//  * Receives a POST request with data of the animal to update, updates db, responds back
-//  * @param  {String} req.params.id - The animalId to update
-//  * @param  {Object} req. An object containing the different attributes of the Animal
-//  * @return {Object} JSON
-//  */
+// a different twilio callback, this one for our meals
+router.post('/twilio-callback2', function(req,res){
 
-router.post('/api/update/:id', function(req, res){
+  // there's lots contained in the body
+  console.log(req.body);
 
-   var requestedId = req.params.id;
+  // the actual message is contained in req.body.Body
+  var incomingMsg = req.body.Body;
+  console.log(incomingMsg);
 
-   var dataToUpdate = {}; // a blank object of data to update
 
-    // pull out the information from the req.body and add it to the object to update
-    var name, age, weight, color, url; 
+  // we don't want to save the entire body as one thing
+  // we want to break it up into fields based on a structure of:
+  // breakfast,3,My House,Brooklyn
+  // which maps to:
+  // type: breakfast
+  // rating: 3,
+  // place: My House
+  // location: Brooklyn
 
-    // we only want to update any field if it actually is contained within the req.body
-    // otherwise, leave it alone.
-    if(req.body.name) {
-      name = req.body.name;
-      // add to object that holds updated data
-      dataToUpdate['name'] = name;
+  // the first thing we need to do is separate the big string into individual parts
+  // we can do that by splitting at the commas
+  var msgArray =  incomingMsg.split(',');
+
+  // now it would look like [breakfast,3,My House,Brooklyn]
+  console.log(msgArray);
+
+  // now we can get the value
+  var type = msgArray[0];
+  var rating = msgArray[1];
+  var place = msgArray[2];
+  var location = msgArray[3];
+
+  // set up our data
+  var mealToSave = {
+    type: type,
+    rating: rating,
+    place: place
+  }
+
+  // now, let's geocode the location
+  geocoder.geocode(location, function (err,data) {
+
+    // set up the twilio response
+    var twilioResp = new twilio.TwimlResponse();
+
+    // if we get an error, or don't have any results, respond back with error
+    if (!data || data==null || err || data.status == 'ZERO_RESULTS'){
+      // respond to user
+      twilioResp.sms('Oops! We couldn\'t save meal.. couldn\'t find location -->'  + location);
+      // respond to twilio
+      res.set('Content-Type', 'text/xml');
+      res.send(twilioResp.toString());
     }
-    if(req.body.age) {
-      age = req.body.age;
-      // add to object that holds updated data
-      dataToUpdate['age'] = age;
-    }
-    if(req.body.weight) {
-      weight = req.body.weight;
-      // add to object that holds updated data
-      dataToUpdate['description'] = {};
-      dataToUpdate['description']['weight'] = weight;
-    }
-    if(req.body.color) {
-      color = req.body.color;
-      // add to object that holds updated data
-      if(!dataToUpdate['description']) dataToUpdate['description'] = {};
-      dataToUpdate['description']['color'] = color;
-    }
-    if(req.body.url) {
-      url = req.body.url;
-      // add to object that holds updated data
-      dataToUpdate['url'] = url;
+
+    // else, let's pull put the lat lon from the results
+    var lon = data.results[0].geometry.location.lng;
+    var lat = data.results[0].geometry.location.lat;
+
+    // now, let's add this to our animal object from above
+    mealToSave.location = {
+      geo: [lon,lat], // need to put the geo co-ordinates in a lng-lat array for saving
+      name: data.results[0].formatted_address // the location name
     }
 
-    var tags = []; // blank array to hold tags
-    if(req.body.tags){
-      tags = req.body.tags.split(","); // split string into array
-      // add to object that holds updated data
-      dataToUpdate['tags'] = tags;
-    }
+      var meal = new Meal(mealToSave)
+
+      meal.save(function(err,data){
+        if(err){
+          // respond to user
+          twilioResp.sms('Oops! We couldn\'t save meal --> ' + incomingMsg);
+          // respond to twilio
+          res.set('Content-Type', 'text/xml');
+          res.send(twilioResp.toString());
+        }
+        else {
+          // respond to user
+          twilioResp.sms('Successfully saved meal! --> ' + incomingMsg);
+          // respond to twilio
+          res.set('Content-Type', 'text/xml');
+          res.send(twilioResp.toString());
+        }
+      })
 
 
-    console.log('the data to update is ' + JSON.stringify(dataToUpdate));
+  });
 
-    // now, update that animal
-    // mongoose method findByIdAndUpdate, see http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate  
-    Animal.findByIdAndUpdate(requestedId, dataToUpdate, function(err,data){
-      // if err saving, respond back with error
-      if (err){
-        var error = {status:'ERROR', message: 'Error updating animal'};
-        return res.json(error);
+
+})
+
+router.get('/api/get',function(req,res){
+
+  Status.find(function(err,data){
+    if(err){
+      var error = {
+        status: "ERROR",
+        message: err
       }
-
-      console.log('updated the animal!');
-      console.log(data);
-
-      // now return the json data of the new person
+      res.json(error);
+    }
+    else {
       var jsonData = {
-        status: 'OK',
-        animal: data
+        status: "OK",
+        statuses: data
       }
-
-      return res.json(jsonData);
-
-    })
-
-})
-
-/**
- * GET '/api/delete/:id'
- * Receives a GET request specifying the animal to delete
- * @param  {String} req.params.id - The animalId
- * @return {Object} JSON
- */
-
-router.get('/api/delete/:id', function(req, res){
-
-  var requestedId = req.params.id;
-
-  // Mongoose method to remove, http://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
-  Animal.findByIdAndRemove(requestedId,function(err, data){
-    if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find that animal to delete'};
-      return res.json(error);
+      res.json(jsonData);
     }
-
-    // otherwise, respond back with success
-    var jsonData = {
-      status: 'OK',
-      message: 'Successfully deleted id ' + requestedId
-    }
-
-    res.json(jsonData);
-
   })
 
 })
+
+router.get('/api/get/latest',function(req,res){
+
+  Status.find().sort('-dateAdded').exec(function(err,data){
+    if(err){
+      var error = {
+        status: "ERROR",
+        message: err
+      }
+      res.json(error);
+    }
+    else {
+      var jsonData = {
+        status: "OK",
+        status: data[0]
+      }
+      res.send(data[0].status);
+    }
+  })
+
+})
+
+
+router.get('/api/get/meals',function(req,res){
+
+  Meal.find(function(err,data){
+    if(err){
+      var error = {
+        status: "ERROR",
+        message: err
+      }
+      res.json(error);
+    }
+    else {
+      var jsonData = {
+        status: "OK",
+        meals: data
+      }
+      res.json(jsonData);
+    }
+  })
+
+})
+
+
 
 module.exports = router;
